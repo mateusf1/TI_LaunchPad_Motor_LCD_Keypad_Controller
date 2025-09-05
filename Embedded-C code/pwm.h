89ad94f3a6ef406e94dcbf14aa66a8c923c809ae
Mateus A. Fernandes A. <64300472+mateusf1@users.noreply.github.com>
2025-09-05 16:50:23 -0400

    Update pwm.h

--- Embedded-C code/pwm.h
+++ Embedded-C code/pwm.h
@@ -2,6 +2,8 @@
  * pwm.h
  *
  * Set-up/init and generates PWM signal
+ *
+ * By: Mateus A. Fernandes A. (GitHum: mateusf1)
  */
 
 #ifndef PWM_H_
@@ -16,3 +18,4 @@ void pwm_init(void);
 void set_duty_cycle(unsigned short duty_cycle);
 
 #endif /* PWM_H_ */
+
